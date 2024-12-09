a3a1f53d95a30348a4dc21f1a87ca77679ced66e
lilong0402 <2199630705@qq.com>
2024-12-09 14:16:56 +0800

    集成redis并测试

--- src/test/java/top/lilong/mall/Test/RedisTest.java
+++ src/test/java/top/lilong/mall/Test/RedisTest.java
@@ -1,21 +1,25 @@
-package top.lilong.mall;
+package top.lilong.mall.Test;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.lilong.mall.util.RedisUtil;
 
+/**
+ * @author Lilong
+ * * @date 2024/12/9
+ * @version 1.0
+ */
 @SpringBootTest
-class MallApplicationTests {
+public class RedisTest {
     @Autowired
     RedisUtil redisUtil;
+
     @Test
     public void contextLoads() {
         redisUtil.set("test:key:name", "xxxxxx");
         redisUtil.set("test:key:name2", "yyyyy");
-
-        redisUtil.delete("test:key:name");
-        System.out.println(redisUtil.getString("test:key:name"));
+        System.out.println(redisUtil.getString("test:key:name2"));
     }
-
 }
